9903d0025b9b7193bde837dca2acc6d4cf90453c
Um1dd <umudnagili9@gmail.com>
2026-04-29 14:55:32 -0400

    Add root process monitoring script

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-ps -u "$1" -f | grep -v " 0      0 "
+
+# Check if argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <username>"
+    exit 1
+fi
+
+USER_NAME=$1
+
+# List processes for the user and filter out VSZ=0 or RSS=0
+ps -u "$USER_NAME" -o user,pid,%cpu,%mem,vsz,rss,tty,stat,start,time,command \
+| awk 'NR==1 || ($5 != 0 && $6 != 0)'
