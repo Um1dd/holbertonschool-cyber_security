3632ec6163ce98f217a545de306865ae36b8407b
Um1dd <umudnagili9@gmail.com>
2026-05-02 02:41:16 -0400

    Add root process monitoring script

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-if [ -z "$1" ]; then
-    echo "Usage: $0 <username>"
-    exit 1
-fi
-
-USER_NAME=$1
-
-ps -u "$USER_NAME" -o user,pid,%cpu,%mem,vsz,rss,tty,stat,start,time,command \
-| grep -v -E '^[^ ]+ +[0-9]+ +[0-9.]+ +[0-9.]+ +0 +| +0 +[^ ]+$'
+ps -u "$1" -f | grep -E "UID|[^0] +[^0]"
