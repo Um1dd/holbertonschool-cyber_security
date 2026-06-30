69522165d25e3f1a1df115cb4b06f07df5b3feee
Um1dd <umudnagili9@gmail.com>
2026-06-30 07:21:39 -0400

    Add README.md and 0-login.sh

--- linux_security/0x00_linux_security_basics/0-login.sh
+++ linux_security/0x00_linux_security_basics/0-login.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-# Check if the script is being run as root
-if [ "$EUID" -ne 0 ]; then
-   echo "Please run as root or use sudo."
-   exit 1
-fi
-
-# Fetch and display the last 5 login sessions
 last -n 5
