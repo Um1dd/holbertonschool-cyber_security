6f29309ca37159f2c0176644d59f6e02d1860b30
Um1dd <umudnagili9@gmail.com>
2026-07-01 01:37:21 -0400

    Fix: Push both 2-line script and the password placeholder file

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1"; john --show "$1" | awk -F: 'NR>1 || $2!="" {print $2}' | sed '/^$/d' > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1"; john --show "$1" | cut -d: -f2 | grep -v "password cracked" | sed '/^$/d' > 4-password.txt
