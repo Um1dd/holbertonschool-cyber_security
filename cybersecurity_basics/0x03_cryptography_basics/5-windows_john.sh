0793e8654372c9e984c2cee0d95dcd1ecad1aff6
Um1dd <umudnagili9@gmail.com>
2026-07-01 01:54:17 -0400

    Fix: Ensure 5-password.txt gets a single clean line output

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --pot=win.pot --format=nt --wordlist=/usr/share/wordlists/rockyou.txt "$1"; john --pot=win.pot --format=nt --show "$1" | cut -d: -f2 | grep -v "password cracked" | tr -d '\r' | sed '/^$/d' > 5-password.txt
+john --format=nt --wordlist=/usr/share/wordlists/rockyou.txt "$1"; john --format=nt --show "$1" | head -n 1 | cut -d: -f2 | tr -d '\r\n ' > 5-password.txt
