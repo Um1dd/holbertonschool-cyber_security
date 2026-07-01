bc1f784b51940f5f4b71a77d136b4086755d8350
Um1dd <umudnagili9@gmail.com>
2026-07-01 01:56:27 -0400

    Fix: Ensure 5-password.txt gets a single clean line output

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --format=nt --wordlist=/usr/share/wordlists/rockyou.txt "$1"; john --format=nt --show "$1" | head -n 1 | cut -d: -f2 | tr -d '\r\n ' > 5-password.txt
+john --format=nt --wordlist=/usr/share/wordlists/rockyou.txt "$1" | awk -F' ' '/\?/ {print $1}' | tr -d '\r\n ' > 5-password.txt
