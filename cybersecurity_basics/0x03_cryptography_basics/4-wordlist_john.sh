4fe31e5920999b1706bf336db4b7bb4bc56d74c2
Um1dd <umudnagili9@gmail.com>
2026-07-01 01:25:12 -0400

    Fix: Make john script dynamic and output directly to 4-password.txt

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 john --wordlist=/usr/share/wordlists/rockyou.txt "$1"
-john --show "$1" | awk -F: 'NR>1 && $2!="" {print $2}' | grep -v "password cracked" > 4-password.txt
+john --show "$1" | cut -d: -f2 > 4-password.txt
